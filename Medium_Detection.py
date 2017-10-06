b137caa5803734a5b85b30ee353b7280f749e554
Zijie ZOU <zouzi.jie@163.com>
2017-10-06 16:42:42 +0800

    Recovery

--- Medium_Detection.py
+++ Medium_Detection.py
@@ -1,47 +1,69 @@
 import cv2
 import numpy as np
 
+temp_b=[]
+temp_g=[]
+temp_r=[]
+
+medium_b=[]
+medium_g=[]
+medium_r=[]
 img = cv2.imread('./cutframe/0.jpg')
-sumlistb = np.zeros((img.shape[0],img.shape[1]),dtype=img.dtype)
-sumlistg = np.zeros((img.shape[0],img.shape[1]),dtype=img.dtype)
-sumlistr = np.zeros((img.shape[0],img.shape[1]),dtype=img.dtype)
-sumb = np.array(sumlistb)
-sumg = np.array(sumlistg)
-sumr = np.array(sumlistr)
-# print(sumb.shape)
-for i in range(0,1012):
-    img = cv2.imread('./cutframe/'+str(i)+'.jpg')
-    b = cv2.split(img)[0]
-    g = cv2.split(img)[1]
-    r = cv2.split(img)[2]
+num=int(len(img))
+j=0
+while j < num:
+    for i in range(0,100):
+        img = cv2.imread('./cutframe/'+str(i)+'.jpg')
+        b = cv2.split(img)[0]
+        g = cv2.split(img)[1]
+        r = cv2.split(img)[2]
 
-    imgb = cv2.GaussianBlur(b,(3,3),0)
-    cannyb = cv2.Canny(imgb, 40, 100,L2gradient=True)  # apertureSize默认为3
-    canny_b=np.array(cannyb)
-    # print(canny_b.shape)
-    sumb=sumb+canny_b
+        # imgb = cv2.GaussianBlur(b,(3,3),0)
+        cannyb = cv2.Sobel(b,cv2.CV_64F,0,1,ksize=5)  # apertureSize默认为3
+        cannyb = cannyb.tolist()
+        temp_b.append(cannyb[j])
 
-    imgg = cv2.GaussianBlur(g, (3, 3), 0)
-    cannyg = cv2.Canny(imgg, 40, 100, L2gradient=True)  # apertureSize默认为3
-    canny_g = np.array(cannyg)
 
-    sumg = sumg + canny_g
+        # imgg = cv2.GaussianBlur(g, (3, 3), 0)
+        cannyg = cv2.Sobel(g,cv2.CV_64F,0,1,ksize=5)  # apertureSize默认为3
+        cannyg = cannyg.tolist()
+        temp_g.append(cannyg[j])
 
-    imgr = cv2.GaussianBlur(r, (3, 3), 0)
-    cannyr = cv2.Canny(imgr, 40, 100, L2gradient=True)  # apertureSize默认为3
-    canny_r = np.array(cannyr)
+        # imgr = cv2.GaussianBlur(r, (3, 3), 0)
+        cannyr = cv2.Sobel(r,cv2.CV_64F,0,1,ksize=5)  # apertureSize默认为3
+        cannyr = cannyr.tolist()
+        temp_r.append(cannyr[j])
 
-    sumr = sumr + canny_r
 
-sumb=sumb/1012
-sumg=sumg/1012
-sumr=sumr/1012
 
-# newb=sumb.tolist()
-# newg=sumg.tolist()
-# newr=sumr.tolist()
+    np_temp_b=np.array(temp_b)
+    np_temp_b.sort(0)
+    np_temp_b=np_temp_b.tolist()
+    medium_b.append(np_temp_b[50])
 
-merged = cv2.merge([sumb,sumg,sumr])
+
+
+    np_temp_g = np.array(temp_g)
+    np_temp_g.sort(0)
+    np_temp_g=np_temp_g.tolist()
+    medium_g.append(np_temp_g[50])
+
+    np_temp_r = np.array(temp_r)
+    np_temp_r.sort(0)
+    np_temp_r=np_temp_r.tolist()
+    medium_r.append(np_temp_r[50])
+
+    j+=1
+
+
+
+medium_b=np.array(medium_b)
+print(medium_b)
+medium_g=np.array(medium_g)
+medium_r=np.array(medium_r)
+
+merged = cv2.merge([medium_b,medium_g,medium_r])
+print(merged)
 cv2.imshow('Canny', merged)
 cv2.imwrite('Result_img.jpg',merged)
 cv2.waitKey(0)
---
+++ canny.py
@@ -0,0 +1,71 @@
+import cv2
+import numpy as np
+
+img = cv2.imread('./0.jpg')
+sumlistb = np.zeros((img.shape[0],img.shape[1]),dtype=img.dtype)
+sumlistg = np.zeros((img.shape[0],img.shape[1]),dtype=img.dtype)
+sumlistr = np.zeros((img.shape[0],img.shape[1]),dtype=img.dtype)
+sumb = np.array(sumlistb)
+sumg = np.array(sumlistg)
+sumr = np.array(sumlistr)
+
+for i in range(0,1012):
+    img = cv2.imread('./cutframe/'+str(i)+'.jpg')
+    b = cv2.split(img)[0]
+    g = cv2.split(img)[1]
+    r = cv2.split(img)[2]
+
+    imgb = cv2.GaussianBlur(b,(3,3),0)
+    cannyb = cv2.Canny(imgb, 40, 100,L2gradient=True)  # apertureSize默认为3
+    canny_b=np.array(cannyb)
+
+    sumb=sumb+canny_b
+
+    imgg = cv2.GaussianBlur(g, (3, 3), 0)
+    cannyg = cv2.Canny(imgg, 40, 100, L2gradient=True)  # apertureSize默认为3
+    canny_g = np.array(cannyg)
+
+    sumg = sumg + canny_g
+
+    imgr = cv2.GaussianBlur(r, (3, 3), 0)
+    cannyr = cv2.Canny(imgr, 40, 100, L2gradient=True)  # apertureSize默认为3
+    canny_r = np.array(cannyr)
+
+    sumr = sumr + canny_r
+
+sumb=sumb/1012
+
+cv2.imshow('Canny', canny)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
+
+
+# !/usr/bin/env python
+# encoding: utf-8
+# import cv2
+# import numpy as np
+#
+#
+# def CannyThreshold(lowThreshold):
+#     detected_edges = cv2.GaussianBlur(gray, (3, 3), 0)
+#     detected_edges = cv2.Canny(detected_edges, lowThreshold, lowThreshold * ratio, apertureSize=kernel_size)
+#     dst = cv2.bitwise_and(img, img, mask=detected_edges)  # just add some colours to edges from original image.
+#     cv2.imshow('canny demo', dst)
+#
+#
+# lowThreshold = 0
+# max_lowThreshold = 100
+# ratio = 3
+# kernel_size = 3
+#
+# img = cv2.imread('./Results/water_y.jpg')
+# gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+#
+# cv2.namedWindow('canny demo')
+#
+# cv2.createTrackbar('Min threshold', 'canny demo', lowThreshold, max_lowThreshold, CannyThreshold)
+#
+# CannyThreshold(0)  # initialization
+# if cv2.waitKey(0) == 27:
+#     cv2.destroyAllWindows()
